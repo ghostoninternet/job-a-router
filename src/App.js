1950299736710173138713e7a9560b22385b38b2
PhucPhuc <daosyphuc288@gmail.com>
2023-08-22 19:51:17 +0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { 
+  createBrowserRouter, 
+  Route, 
+  RouterProvider,
+  createRoutesFromElements
+} from "react-router-dom"
+
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+import RootLayout from "./layouts/RootLayout";
+import HelpLayout from "./layouts/HelpLayouts";
+import Contact, { contactAction } from "./pages/help/Contact";
+import Faq from "./pages/help/Faq";
+import NotFound from "./pages/NotFound";
+import CareersLayout from "./layouts/CareersLayouts";
+import Careers, { careersLoader } from "./pages/career/Career";
+import CareerDetails, { careerDetailsLoader } from "./pages/career/CareerDetails";
+import CareersError from "./pages/career/CareerError";
+
+const router = createBrowserRouter(
+  createRoutesFromElements (
+    <Route path="/" element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="help" element={<HelpLayout />}>
+            <Route path="faq" element={<Faq />} />
+            <Route path="contact" element={<Contact />} action={contactAction}/>
+          </Route>
+          <Route path="careers" element={<CareersLayout />} errorElement={<CareersError />} >
+            <Route
+              index
+              element={<Careers />}
+              loader={careersLoader}
+              />
+            <Route
+              path=":id"
+              element={<CareerDetails />}
+              loader={careerDetailsLoader}
+              />
+          </Route>
+
+          <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <RouterProvider router={router} />
   );
 }
 
-export default App;
+export default App
